696525ddaa7a9180fe07671b2d89389a7c058a5d
veratech <jared@veratech.co.nz>
2017-11-17 10:58:18 +1300

    latest

---
+++ class/11-17-2017 json con't and sorting data/task-b.js
@@ -0,0 +1,11 @@
+// 1.***
+// Create 3 seperate tables that can be switched via ui switch button without a page reload
+// Each table should represent some data from the json place holder api ( The data and url for each table is your choice)
+// The switch between each table should re-call/access the json api/server each time (update data)
+
+// 2.***
+// For each table provide a button which based on any column of your choosing
+// re-orders the table by either alpha or numeric order. Buttons such as 'a-z' and
+// 'ascending - descending' should be in play.
+
+// **Make all ui transitions and data updates smooth and visually pleasing  
